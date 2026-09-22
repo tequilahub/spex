d72723be6ea629a78a75cbdd02703a0a5d61d980
Davide Bincoletto <davibinco@gmail.com>
2026-09-22 15:58:23 +0200

    Keep the version at 1.1.0
    1.1.0 was tagged but never published to PyPI, so the number is still
    free. Fold the inner_product fix into it rather than burning 1.1.1 on a
    release nobody could have installed.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ ext_modules = [
 
 setup(
     name="spex-tequila",
-    version="1.1.1",
+    version="1.1.0",
     author="Michael Lang, Julian Bauer and spex developer",
     author_email="",
     url="https://github.com/tequilahub/spex",
